151bf783e8a6dd2d99f1bd9677f72f9154f27b5f
Kshitiz Raj <kshtz.pesce@gmail.com>
2021-06-26 11:23:43 +0530

    Added Team Controller

---
+++ src/main/java/com/kshtzraj/ipldashboard/Controller/TeamController.java
@@ -0,0 +1,32 @@
+package com.kshtzraj.ipldashboard.Controller;
+
+import com.kshtzraj.ipldashboard.model.Team;
+import com.kshtzraj.ipldashboard.repository.MatchRepository;
+import com.kshtzraj.ipldashboard.repository.TeamRepository;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TeamController {
+    
+    private TeamRepository teamRepository;
+    private MatchRepository matchRepository;
+    
+
+    public TeamController(TeamRepository teamRepository, MatchRepository matchRepository) {
+        this.teamRepository = teamRepository;
+        this.matchRepository = matchRepository;
+    }
+
+
+
+    @GetMapping("/team/{teamName}")
+    public Team getTeam(@PathVariable String teamName) {
+        Team team = this.teamRepository.findByTeamName(teamName);
+        team.setMatches(matchRepository.findLatestMatchesbyTeam(teamName, 4));
+        return team;
+    }
+
+}
